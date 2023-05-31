f51561d8ee6981a2bde20967c34e584c24f0283e
Tu Nombre <tu@email.com>
2023-05-31 11:30:31 -0600

    static

--- Deployment/wsgi.py
+++ Deployment/wsgi.py
@@ -11,7 +11,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Deployment.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
